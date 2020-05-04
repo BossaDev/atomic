27b9fb2d235979a13e9e84d967c9c0f62e14a2ef
fabiohild <fabiohildebrand@gmail.com>
2020-05-04 20:15:43 -0300

    xml 2 json

--- core/zoom_controls.js
+++ core/zoom_controls.js
@@ -330,8 +330,41 @@ Blockly.ZoomControls.prototype.createLaunchSvg_ = function () {
 
   // Attach event listeners.
   Blockly.bindEventWithChecks_(launchSvg, 'mousedown', null, function (e) {
-    console.log(Blockly.Xml.workspaceToDom(ws))
+
+    console.log(xmlToJson(Blockly.Xml.workspaceToDom(ws)))
     Blockly.Touch.clearTouchIdentifier(); // Don't block future drags.
     e.preventDefault(); // Stop double-clicking from selecting text.
   });
 };
+
+var xmlToJson = function (xml) {
+  var obj = {};
+  if (xml.nodeType == 1) {
+    if (xml.attributes.length > 0) {
+      obj["@attributes"] = {};
+      for (var j = 0; j < xml.attributes.length; j++) {
+        var attribute = xml.attributes.item(j);
+        obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+      }
+    }
+  } else if (xml.nodeType == 3) {
+    obj = xml.nodeValue;
+  }
+  if (xml.hasChildNodes()) {
+    for (var i = 0; i < xml.childNodes.length; i++) {
+      var item = xml.childNodes.item(i);
+      var nodeName = item.nodeName;
+      if (typeof (obj[nodeName]) == "undefined") {
+        obj[nodeName] = xmlToJson(item);
+      } else {
+        if (typeof (obj[nodeName].push) == "undefined") {
+          var old = obj[nodeName];
+          obj[nodeName] = [];
+          obj[nodeName].push(old);
+        }
+        obj[nodeName].push(xmlToJson(item));
+      }
+    }
+  }
+  return obj;
+}
