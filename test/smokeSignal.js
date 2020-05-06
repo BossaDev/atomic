775e0cbd61a6019365523577d75365f8cbde1464
fabiohild <fabiohildebrand@gmail.com>
2020-05-06 19:03:44 -0300

    finalizing eth transfer tests

--- test/smokeSignal.js
+++ test/smokeSignal.js
@@ -1,16 +1,17 @@
 const {
-    expect
+    expect,
+    assert
 } = require("chai");
 
 var ethers = require("ethers");
 
-
+// encoder from the block
 let encoder = function (value, unit, to) {
     // encoding for atomic
     let encoder = new ethers.utils.AbiCoder();
     let types = ["address", "uint256", "bytes"]; // to, value, data
 
-    return encoder.encode(types, [to, value, "0x0"]).slice(2);
+    return encoder.encode(types, [to, ethers.utils.parseUnits(value,unit).toString(), "0x0"]).slice(2);
 }
 
 const ganacheUrl = "http://127.0.0.1:8545";
@@ -18,17 +19,7 @@ let provider = new ethers.providers.JsonRpcProvider(ganacheUrl);
 // ganache-cli -d -f https://cloudflare-eth.com
 
 const signer = provider.getSigner(0);
-const signer1 = provider.getSigner(1);
 
-let tx1 = encoder("10000000", 0, "0x12C4914c27B8A9A038E16104d06e8679c4eD8Dc6")
-let tx2 = encoder("10000000000000000000", 0, "0xaAF6da1c0e9CF6Fc7F18C5d648337e32EFbdE718")
-let tx3 = encoder("20000000000000000000", 0, "0xC69E1e6E3A8296Bb1b21158bA0C6c3447F5339e5")
-
-
-// encoding for atomic
-let payload = "0x" + tx1
-
-// lift off
 let atomicAbi = [{
     "inputs": [{
         "internalType": "bytes",
@@ -43,24 +34,37 @@ let atomicBytecode = "0x60806040526040516101a63803806101a68339818101604052602081
 let factory = new ethers.ContractFactory(atomicAbi, atomicBytecode, signer);
 
 
-
-
 describe("Eth_Transfer", function () {
-    it("Should return the new greeting once it's changed", async function () {
-        console.log("Hey");
-        console.log((await provider.getBalance("0x12C4914c27B8A9A038E16104d06e8679c4eD8Dc6")).toString())
-        console.log(payload)
-        // let test = new Uint8Array(payload)
-        // console.log(test)
+    it("Should transfer ether", async function () {
+
+        let txs = [
+            ["1", "ether", "0x12C4914c27B8A9A038E16104d06e8679c4eD8Dc6"],
+            ["20000000", "wei", "0xaAF6da1c0e9CF6Fc7F18C5d648337e32EFbdE718"],
+            ["300", "gwei", "0xC69E1e6E3A8296Bb1b21158bA0C6c3447F5339e5"]
+        ]
+
+        let payload = "0x"
+
+        let previousBalances = []
+
+        for (i = 0; i < 3; i++) {
+            previousBalances[i] = await provider.getBalance(txs[i][2])
+            payload += encoder(txs[i][0], txs[i][1], txs[i][2])
+        }
+
+        // atomic launch with payload
         let contract = await factory.deploy(payload, {
-            value: "0x10000000000",
+            value: ethers.utils.parseUnits("2", "ether").toHexString(),
             gasPrice: 1,
             gasLimit: 6721975
         });
 
-        console.log("Depdloyed at", contract.address);
-        console.log("Tx Hash", contract.deployTransaction.hash);
+        // console.log("Depdloyed at", contract.address);
+        // console.log("Tx Hash", contract.deployTransaction.hash);
 
-        console.log((await provider.getBalance("0x12C4914c27B8A9A038E16104d06e8679c4eD8Dc6")).toString())
+        for (i = 0; i < 3; i++) {
+            currentBalance = await provider.getBalance(txs[i][2])
+            assert.equal(previousBalances[i].add(ethers.utils.parseUnits(txs[i][0],txs[i][1])).toString(), currentBalance.toString())
+        }
     });
 });
