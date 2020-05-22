0b651dba2bd53a9024faa343c42935eff33fe37a
fabiohild <fabiohildebrand@gmail.com>
2020-05-22 15:05:00 -0300

    new atomic contract working

--- contracts/Atomic.sol
+++ contracts/Atomic.sol
@@ -9,12 +9,10 @@ interface IERC20 {
 contract AtomicProxy {
     address payable public owner; 
     address payable public factory;
-    uint time;
     
     constructor() public payable {
         owner = tx.origin;
         factory = msg.sender;
-        time = now;
     }
     
     receive() external payable {
@@ -22,31 +20,44 @@ contract AtomicProxy {
     }
 
     fallback () external payable {
-        // (bool success, ) = factory.delegatecall(msg.data);
-        // if (!success) {
-        //     assembly {
-        //         returndatacopy(0, 0, returndatasize())
-        //         revert(0, returndatasize())
-        //     }
-        // }
+        (bool success, ) = factory.delegatecall(msg.data);
+        if (!success) {
+            assembly {
+                returndatacopy(0, 0, returndatasize())
+                revert(0, returndatasize())
+            }
+        }
     }
 }
 
 contract Atomic {
     address payable public owner = msg.sender;
     address payable public factory; //needed to keep the storage layout equal to AtomicProxy
-    uint public time = now;
     
     function launchAtomic(address[] calldata _to, uint[] calldata _value, bytes[] calldata _data) external payable returns (bool success) {
         require (_to.length == _value.length && _value.length == _data.length, "Parameters are incorrect");
         AtomicProxy atomicProxy = new AtomicProxy();
-        Atomic atomicInterface = Atomic(address(atomicProxy));
-        success = atomicInterface.execute{value: msg.value}(_to, _value, _data);
-        atomicInterface.drain(address(0));
+        // Atomic atomicInterface = Atomic(address(atomicProxy));
+        factory = address(atomicProxy);
+        bytes memory txData = abi.encodeWithSelector(
+                Atomic.execute.selector,
+                _to,
+                _value,
+                _data
+            );
+        (success, ) = address(atomicProxy).call{value: msg.value}(txData);
+        if (!success) {
+            assembly {
+                returndatacopy(0, 0, returndatasize())
+                revert(0, returndatasize())
+            }
+        }
+        // success = atomicInterface.execute{value: msg.value}(_to, _value, _data);
+        // atomicInterface.drain(address(0));
     }
     
     function execute(address[] calldata _to, uint[] calldata _value, bytes[] calldata _data) payable external returns (bool success) {
-        require (time == now, "Execution can only be performed once");
+        // require (time == now, "Execution can only be performed once");
         for (uint i = 0; i < _data.length; i++) {
             (success, ) = payable(_to[i]).call{value: _value[i]}(_data[i]);
             if (!success) {
@@ -65,5 +76,9 @@ contract Atomic {
             IERC20 token = IERC20(_token);
             token.transfer(owner, token.balanceOf(address(this)));
         }
+    }
+    
+    fallback () external payable {
+        revert("Not correctly encoded for execute or drain.");
     }
 }
