eaa31106efd406a70751fc563c809382abcc1dcd
fabiohild <fabiohildebrand@gmail.com>
2020-08-08 21:10:55 -0300

    2nd hackathon submission

--- js/saveAtomic.js
+++ js/saveAtomic.js
@@ -8,7 +8,7 @@ const copyToClipboard = str => {
     el.select();
     document.execCommand('copy');
     document.body.removeChild(el);
-  };
+};
 
 const saveAtomic = () => {
     let xml = Blockly.Xml.workspaceToDom(Blockly.mainWorkspace)
@@ -20,13 +20,25 @@ const saveAtomic = () => {
 const openAtomic = (txName) => {
     // let txName = prompt("Open transaction", "My Atomic Tx");
     let xml = Blockly.Xml.textToDom(localStorage.getItem(txName))
-    console.log(xml)
 
     Blockly.mainWorkspace.clear()
     Blockly.Xml.domToWorkspace(Blockly.mainWorkspace, xml)
 }
 
+var modal
+
 const openMenu = () => {
+    let list = JSON.parse(localStorage.getItem('txList'));
+
+    let modalContent = "<h2>Open Transaction</h2>"
+    for (i = 0; i < list.length; i++)
+        modalContent += "<h4><a href='#' onclick='openAtomic(\"" + list[i] + "\");modal.close()'> " + list[i] + "</a><h4>"
+
+    modal = picoModal(modalContent).show();
+    return
+
+    console.log(list)
+
     let txName = prompt("Please type the name of the tx to open (we'll improve this soon).", "My Atomic Tx");
     openAtomic(txName)
 }
@@ -55,6 +67,30 @@ const shareAtomic = () => {
     });
 }
 
+const embedAtomic = () => {
+    const ipfs = IpfsHttpClient('ipfs.infura.io', '5001', {
+        protocol: "https"
+    })
+
+    let xml = Blockly.Xml.workspaceToDom(Blockly.mainWorkspace)
+
+    const transaction = buffer.Buffer(Blockly.Xml.domToText(xml))
+    ipfs.add(transaction, (err, result) => {
+        if (err) {
+            console.error(err)
+            return
+        } else {
+            console.log(result)
+            console.log('https://ipfs.io/ipfs/' + result[0].hash)
+            copyToClipboard("https://atomic.ninja/?&tx=" + result[0].hash + "&embed=true")
+            alert("URL copied to clipboard. iFrame it on your webpage and you'll be good to go (SSL required). \n\n It's on IPFS, so if you want it to persist overtime you have to pin it. We recomment https://pinata.cloud/, they allow for pinning of thousasnds of transactions in their free tier. That said, your transaction should be good for the coming days. \n\n The IPFS hash of the transaction is: " + result[0].hash)
+
+            // copying to clipboard
+
+        }
+    });
+}
+
 function addToTxList(txName) {
     var list;
     //is anything in localstorage?
@@ -63,6 +99,7 @@ function addToTxList(txName) {
     } else {
         list = JSON.parse(localStorage.getItem('txList'));
     }
-    list.push(txName);
+    if (!list.includes(txName))
+        list.push(txName);
     localStorage.setItem('txList', JSON.stringify(list));
 }
--- js/setup.js
+++ js/setup.js
@@ -1,3 +1,7 @@
+'use strict';
+
+var workspace = null;
+
 // routines that run when starting
 function getUrlVars() {
   var vars = {};
@@ -7,17 +11,70 @@ function getUrlVars() {
   return vars;
 }
 
-
+function getToolboxElement() {
+  var match = location.search.match(/toolbox=([^&]+)/);
+  return document.getElementById('toolbox-' + (match ? match[1] : 'categories'));
+}
 
 async function setup() {
+
+
   let urlVars = getUrlVars()
 
   const ipfs = IpfsHttpClient('ipfs.infura.io', '5001', {
     protocol: "https"
   })
 
+  let readOnly = false
+  let scrollbars = true
+  if (urlVars.embed) {
+    // hide stuff
+    document.getElementById("navbar").style.display = "none";
+    document.getElementById("blocklyDiv").style.height = "100%";
+    document.getElementById("blocklyDiv").style.top = "-30px";
+    document.getElementById("embedLink").href = "https://atomic.ninja?&tx=" + urlVars.tx;
+    readOnly = true
+    scrollbars = false
+  } else {
+    document.getElementById("embedLogo").style.display = "none";
+  }
+
+  // Create main workspace.
+  workspace = Blockly.inject('blocklyDiv', {
+    comments: true,
+    disable: false,
+    collapse: false,
+    media: './media/',
+    readOnly: readOnly,
+    rtl: null,
+    scrollbars: scrollbars,
+    toolbox: getToolboxElement(),
+    toolboxPosition: "start",
+    horizontalLayout: false,
+    sounds: true,
+    zoom: {
+      controls: true,
+      wheel: true,
+      startScale: 0.85,
+      maxScale: 4,
+      minScale: 0.25,
+      scaleSpeed: 1.1
+    },
+    colours: {
+      fieldShadow: 'rgba(255, 255, 255, 0.3)',
+      dragShadowOpacity: 0.6
+    }
+  });
+
+  // open tx
   if (urlVars.tx) {
-    txData = await ipfs.get(urlVars.tx)
+
+    let txData
+    if (urlVars.tx.substr(-1) == "#")
+      txData = await ipfs.get(urlVars.tx.slice(0, -1))
+    else
+      txData = await ipfs.get(urlVars.tx)
+
 
     let atomicTx = new TextDecoder("utf-8").decode(txData[0].content);
 
@@ -25,12 +82,18 @@ async function setup() {
     Blockly.Xml.domToWorkspace(Blockly.mainWorkspace, xml)
   }
 
-  if (urlVars.embed) {
-    // hide stuff
-    console.log("hide stuff")
-  }
+  if (!urlVars.embed) 
+  picoModal(
+    "<h2>Welcome to Atomic Ninja!</h2>" +
+    "<p>Send complex atomic transactions to Ethereum like never before.</p>" +
+    "<h3>Quick Start</h3>" +
+    "<p>On the left of the screen there is a list of sorted, disconnected blocks. This is your <b>toolbox</b>.</p>" +
+    "<p>Drag the blocks to the <b>workspace</b> at the right, join them and edit their parameters to perform the actions you´d like.</p>" +
+    "<p>Start with a hat shaped block such as <img src='./media/hatblock.png' style='width: 11%;'>, as they indicate the beginning of your transaction.</p>" +
+    "<p>Once you are done, press <img src='./media/zoom-simulateAtomic.svg'> to simulate your transaction, and <img src='./media/zoom-launchAtomic.svg'> to send it out!</p>" +
+    "<p><b>Tip:</b> Hover the blocks for details on how they work.</p>" +
+    "<p>For more details, head over to the <a href='./docs.html'>Atomic documentation</a>.</p>"
+  ).show();
 
 
 }
-setup()
-
